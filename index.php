22b48da7848fd3c1f8958e58611b0ae6e2f22f33
unknown <caissani2@outlook.com>
2025-11-21 13:14:03 +0100

    finale2

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
  
-function connecter_base_de_donnees()
-{
-    try {
-        $mysqlClient = new PDO(
-            'mysql:host=localhost;dbname=airbnb;charset=utf8',
-            'root',
-            ""
-        );
-        $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $mysqlClient;
-    } catch (PDOException $e) {
-        die('Erreur de connexion à la base de données : ' . $e->getMessage());
-    }
-}
- 
- 
-$status_message = '';
-$db = connecter_base_de_donnees();
+require 'config.php';
  
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
  
